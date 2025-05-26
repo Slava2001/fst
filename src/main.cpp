27a3269e11e28a6a6c2a0d06e5cbd888d11fb7b7
Slava2001 <slavakaplya20011501@gmail.com>
2025-05-26 23:13:18 +0300

    add word iterator

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
+#include "nlohmann/json.hpp"
 
 #define LOG_LVL LogLevel::Debug
 #include "logger.h"
-#include "nlohmann/json.hpp"
+#include "word_iter.h"
 
 int main(int /*unused*/, char** /*unused*/) {
     Logger::init();
     log_enter();
     log_info("Starting the FST...");
+
+    std::string text = "hello, world! snake_case, camelCase, PascalCase, kebab-case, UPPER_CASE";
+    std::istringstream str(text);
+    Words words(str);
+    for (auto word = words.begin(); word != words.end(); ++word) {
+        log_info("Word: ", *word);
+    }
 
     return EXIT_SUCCESS;
 }
---
+++ src/word_iter.cpp
@@ -0,0 +1,56 @@
+#include "word_iter.h"
+// #define LOG_LVL LogLevel::Debug
+#include "logger.h"
+
+WordIterator::WordIterator() : _stream(nullptr), _end_reached(true) {}
+
+WordIterator::WordIterator(std::istream& is) : _stream(&is), _end_reached(false) { ++(*this); }
+
+const std::string& WordIterator::operator*() const { return _current_word; }
+
+const std::string* WordIterator::operator->() const { return &_current_word; }
+
+WordIterator& WordIterator::operator++() {
+    log_enter();
+    _current_word.clear();
+    while (!_end_reached) {
+        char tmp;
+        _stream->get(tmp);
+        if (_stream->bad()) {
+            log_error("Stream error occurred: ", _stream->rdstate());
+            throw std::ios_base::failure("Stream error occurred");
+        } else if (_stream->eof()) {
+            _end_reached = _current_word.empty();
+            break;
+        }
+
+        if (std::isalpha(tmp)) {
+            _current_word += tmp;
+        } else {
+            if (!_current_word.empty()) {
+                break;
+            }
+        }
+    }
+    log_debug("Current word: '", _current_word,
+              "', end reached: ", _end_reached ? "true" : "false");
+    return *this;
+}
+
+WordIterator WordIterator::operator++(int) {
+    WordIterator temp = *this;
+    ++(*this);
+    return temp;
+}
+
+bool WordIterator::operator==(const WordIterator& other) const {
+    return _end_reached == other._end_reached;
+}
+
+bool WordIterator::operator!=(const WordIterator& other) const { return !(*this == other); }
+
+Words::Words(std::istream& is) : _is(is) {}
+
+WordIterator Words::begin() { return WordIterator(_is); }
+
+WordIterator Words::end() { return WordIterator(); }
